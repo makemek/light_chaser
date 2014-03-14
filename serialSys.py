cfa8184c96aefb63329d7a763084ef812d8b94be
Makemek <makemek101@hotmail.com>
2014-03-14 23:31:50 +0700

    Implement 'PortView' ui

--- serialSys.py
+++ serialSys.py
@@ -3,27 +3,33 @@
 from subjectObserver import *
 from PySide.QtGui import *
 
-
-from serial import Serial
+import serial as PySerial
 from view import *
 import time
+import os
 
 class SerialController(Observer):
     
     def __init__(self, view):
-        self.__port = Serial()
+        self.__port = PySerial.Serial()
         self.__view = view
         self.__view.setConnectButtonListener(self.openPort)
 
     def openPort(self):
         print("Open port")
         comName = self.__view.getPort()
-        self.__port.setPort(comName)
-        self.__port.setBaudrate(9600)
-        self.__port.open()
-        #self.__port.setTimeout(2)
-        time.sleep(1)
-        self.__view.setConnect(True)
+        if len(comName) == 0:
+            return
+        
+        try:
+            self.__port.setPort(comName)
+            self.__port.setBaudrate(9600)
+            self.__port.open()
+            self.__port.setTimeout(2)
+            
+            self.__view.setConnect(True)
+        except PySerial.serialutil.SerialException:
+            QErrorMessage("Unable to open port: + " + comName)
 
     def closePort(self):
         self.sendByte(0)
@@ -54,11 +60,12 @@ class SerialView(View):
         print("getPort()")
         
         ui = PortView(self.__parent)
+        ui.activateWindow()
         ui.show()
         
-        # implement prompt gui ask for COM port
         print("After")
-        return ""
+
+        return ui.getPort()
 
     def setConnect(self, isConnect):
         connectMsg = "Connected"
@@ -73,10 +80,69 @@ class SerialView(View):
     def setConnectButtonListener(self, func):
         self.__connectBt.clicked.connect(func)
 
-class PortView(QDialog):
+class PortView(QDialog, View):
     
     def __init__(self, parent=None):
         super(PortView, self).__init__(parent)
-        self.setWindowTitle("Select Port")
 
-        # implement UI
+        self.setWindowTitle("Select Port")
+        self.createUi()
+        self.refreshPort()
+
+    def createUi(self):
+        self.__portList = QComboBox()
+        self.__message = QLabel("Port : ")
+        self.__okBt = QPushButton("OK")
+        
+        self.__refreshBt = QPushButton("Refresh")
+        self.__refreshBt.clicked.connect(self.refreshPort)
+        
+        portLayout = QHBoxLayout()
+        portLayout.addWidget(self.__message)
+        portLayout.addWidget(self.__portList)
+        
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(self.__okBt)
+        buttonLayout.addWidget(self.__refreshBt)
+        
+        mainLayout = QVBoxLayout()
+        mainLayout.addLayout(portLayout)
+        mainLayout.addLayout(buttonLayout)
+        
+        self.setLayout(mainLayout)
+
+    def getPort(self):
+        return self.__portList.currentText()
+
+    def refreshPort(self):
+        self.__portList.clear()
+        for port in self.__serial_ports():
+            self.__portList.addItem(port)
+
+        if self.__portList.count() == 0:
+            self.__okBt.setEnabled(False)
+
+        else:
+            self.__okBt.setEnabled(True)
+
+    def __serial_ports(self):
+        '''
+        Returns a generator for all available serial ports
+        '''
+        if os.name == 'nt':
+            # windows
+            for i in range(256):
+                try:
+                    s = PySerial.Serial(i)
+                    s.close()
+                    yield 'COM' + str(i + 1)
+                except PySerial.serialutil.SerialException:
+                    pass
+        else:
+            # unix
+            for port in list_ports.comports():
+                yield port[0]
+
+
+
+    
