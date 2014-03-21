dfc34c4cf0f0efec8615638fbe12ff02fe473d42
Makemek <makemek101@hotmail.com>
2014-03-22 00:33:19 +0700

    create mediator class and its methods.

---
+++ mediator.py
@@ -0,0 +1,32 @@
+from serialSys import SerialView
+from colorSys import ColorView
+from effectSys import EffectView
+
+class Mediator:
+    
+    def __init__(self):
+        pass
+
+    def registerSerialView(self, view):
+        pass
+
+    def registerTargetColorView(self):
+        pass
+
+    def registerCurrentColorView(self):
+        pass
+
+    def registerEffectView(self):
+        pass
+
+    def serialReady(self):
+        pass
+
+    def colorChanged(self):
+        pass
+
+    def enableRandomize(self):
+        pass
+
+    def enableSmooth(self):
+        pass
